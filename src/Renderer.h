ae73b6e26213ed346c9f3a86211d686d73269ff4
Mar-Dev-One <alahyane.marouan@gmail.com>
2026-07-26 19:20:31 +0100

    fixing bugs

--- src/Renderer.h
+++ src/Renderer.h
@@ -7,14 +7,14 @@
 
 typedef struct renderer
 {
+    window current_window_storage;
     window* current_window;
-
 } renderer;
 
 b8 renderer_init(renderer* renderer);
 
-b8 set_target_window(renderer* renderer, window* window);
+b8 _set_target_window(renderer* renderer, window* window);
 
-b8 renderer_on_update(renderer* renderer, f32 delta_time);
+void renderer_on_update(renderer* renderer, f32 delta_time);
 
 void renderer_destroy(renderer* renderer);
