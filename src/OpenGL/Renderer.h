13b9a16fec6ca99f62baa481205544061cb29b18
Mar-Dev-One <alahyane.marouan@gmail.com>
2026-08-16 01:55:16 +0100

    reafactor; moving the update function into app

--- src/OpenGL/Renderer.h
+++ src/OpenGL/Renderer.h
@@ -15,6 +15,11 @@ b8 init_renderer(window_descriptor init_window_desc);
 
 renderer get_renderer();
 
-void renderer_on_update();
+void renderer_begin_frame();
+void renderer_end_frame();
+
+void renderer_set_viewport(uint16 x, uint16 y, uint16 width, uint16 height);
+
+void renderer_clear(f32 r, f32 g, f32 b, f32 a);
 
 void terminate_renderer();
